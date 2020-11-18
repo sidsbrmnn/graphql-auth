2a8f8f39fe878cc73fda2ec48e99f8aba6e457a3
Siddharth Subramanian <sidsbrmnn@gmail.com>
2020-11-18 19:07:26 +0530

    Add authentication check to queries
    Signed-off-by: Siddharth Subramanian <sidsbrmnn@gmail.com>

---
+++ src/middlewares.js
@@ -0,0 +1,3 @@
+export const isAuthenticated = (context) => {
+    return context.user !== null;
+};
--- src/resolvers.js
+++ src/resolvers.js
@@ -1,16 +1,30 @@
+import { AuthenticationError } from 'apollo-server-express';
 import jwt from 'jsonwebtoken';
 import { articles, authors } from './data';
+import { isAuthenticated } from './middlewares';
 
 export default {
     Query: {
-        articles(parent, { authorId }) {
+        articles(parent, { authorId }, context) {
+            if (!isAuthenticated(context)) {
+                throw new AuthenticationError(
+                    'you must be logged in to query this schema'
+                );
+            }
+
             const filtered = articles.filter(
                 (article) => article.authorId === authorId
             );
 
             return filtered.length > 0 ? filtered : undefined;
         },
-        article(parent, { id }) {
+        article(parent, { id }, context) {
+            if (!isAuthenticated(context)) {
+                throw new AuthenticationError(
+                    'you must be logged in to query this schema'
+                );
+            }
+
             return articles.find((article) => article.id === id);
         },
     },
